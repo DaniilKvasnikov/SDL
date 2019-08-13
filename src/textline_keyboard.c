8ef4172eb7811d6d822b805f340ee7838574443b
rrhaenys <123yk123@mail.ru>
2019-08-13 09:23:04 +0300

    input text

--- src/textline_keyboard.c
+++ src/textline_keyboard.c
@@ -6,7 +6,7 @@
 /*   By: rrhaenys <rrhaenys@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 08:54:25 by rrhaenys          #+#    #+#             */
-/*   Updated: 2019/08/13 09:05:06 by rrhaenys         ###   ########.fr       */
+/*   Updated: 2019/08/13 09:22:13 by rrhaenys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,24 @@ int
 {
 	t_win		*win;
 	t_element	*but;
+	char		*str;
 
 	win = (t_win *)win_ptr;
 	but = (t_element *)but_ptr;
-	if(ev->type == SDL_TEXTINPUT)
+	if(ev->type == SDL_TEXTINPUT && ft_strlen(ev->text.text) == 1)
 	{
-		ft_printf("input : %c\n", ev->text.text);
+		if (but->str == NULL)
+		{
+			but->str = ft_strnew(2);
+			but->str[0] = ev->text.text[0];
+		}
+		else
+		{
+			str = ft_strnew(ft_strlen(but->str) + 2);
+			ft_strcpy(str, but->str);
+			str[ft_strlen(str)] = ev->text.text[0];
+			free(but->str);
+			but->str = str;
+		}
 	}
 }
