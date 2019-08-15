403800a2a4b4f56a591b24649c1bd3954aed933c
rrhaenys <123yk123@mail.ru>
2019-08-15 12:34:45 -0700

    dropdownlist index

--- src/input/dropdownlist_touch.c
+++ src/input/dropdownlist_touch.c
@@ -6,22 +6,23 @@
 /*   By: rrhaenys <rrhaenys@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/15 09:19:01 by rrhaenys          #+#    #+#             */
-/*   Updated: 2019/08/15 12:14:06 by rrhaenys         ###   ########.fr       */
+/*   Updated: 2019/08/15 12:25:47 by rrhaenys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sdl.h"
 
 static int
-	groupe_touch(t_group_e *group, void *win, SDL_Event *ev, t_point_int *mouse)
+	groupe_touch(t_element *elem, void *win, SDL_Event *ev, t_point_int *mouse)
 {
 	int	i;
 
 	i = -1;
-	while (++i < group->count)
-		if (chech_input_mouse((t_element *)group->elements[i], mouse))
+	while (++i < elem->sub_group->count)
+		if (chech_input_mouse((t_element *)elem->sub_group->elements[i], mouse))
 		{
-			push_text_to_parent(win, (t_element *)group->elements[i], ev);
+			elem->int_par = i + 1;
+			push_text_to_parent(win, (t_element *)elem->sub_group->elements[i], ev);
 			return (1);
 		}
 	return (0);
@@ -32,7 +33,7 @@ int
 {
 	ft_putendl("dropdownlist touch 1");
 	if (((t_element *)but)->sub_group &&
-		groupe_touch(((t_element *)but)->sub_group, win, ev, mouse) == 1)
+		groupe_touch(((t_element *)but), win, ev, mouse) == 1)
 		return (0);
 	ft_putendl("dropdownlist touch 2");
 	if (!chech_input_mouse(but, mouse))
