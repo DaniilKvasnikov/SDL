a5491cae0c952551a637100fe689632307fe7f18
Ilya Baturin <ilyabaturin@iMac-Ilya.local>
2019-08-26 16:38:48 +0300

    curr type save

--- libui/src/save_file.c
+++ libui/src/save_file.c
@@ -12,15 +12,55 @@
 
 #include "ft_sdl.h"
 
+static int
+	select_file_type(t_win *win)
+{
+	const SDL_MessageBoxButtonData buttons[] = {
+        { 0, PNG, "PNG" },
+        { 0, BMP, "BMP" },
+        { 0, JPG, "JPG" }
+    };
+    const SDL_MessageBoxColorScheme colorScheme = {
+        { /* .colors (.r, .g, .b) */
+            /* [SDL_MESSAGEBOX_COLOR_BACKGROUND] */
+            { g_sdl_data->color_back.r, g_sdl_data->color_back.g, g_sdl_data->color_back.b},
+            /* [SDL_MESSAGEBOX_COLOR_TEXT] */
+            {   255, 0,   0 },
+            /* [SDL_MESSAGEBOX_COLOR_BUTTON_BORDER] */
+            { 0, 255,   0 },
+            /* [SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND] */
+            {   0,   0, 255 },
+            /* [SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED] */
+            { 255,   255, 255 }
+        }
+    };
+    const SDL_MessageBoxData messageboxdata = {
+        SDL_MESSAGEBOX_INFORMATION, /* .flags */
+        NULL, /* .window */
+        "Type of file", /* .title */
+        "Select type of file", /* .message */
+        SDL_arraysize(buttons), /* .numbuttons */
+        buttons, /* .buttons */
+        &colorScheme /* .colorScheme */
+    };
+    int buttonid;
+    if (SDL_ShowMessageBox(&messageboxdata, &buttonid) < 0) {
+        SDL_Log("error displaying message box");
+        return 0;
+    }
+    return buttonid;
+}
+
 void
 	save_file(t_win *win, int type, int num_layer, char *path)
 {
 	SDL_Texture* target = SDL_GetRenderTarget(win->ren);
-    SDL_SetRenderTarget(win->ren, win->layers[num_layer]->texture);
-    int width, height;
-    SDL_QueryTexture(win->layers[num_layer]->texture, NULL, NULL, &width, &height);
-    SDL_Surface* surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
-    SDL_RenderReadPixels(win->ren, NULL, surface->format->format, surface->pixels, surface->pitch);
+	SDL_SetRenderTarget(win->ren, win->layers[num_layer]->texture);
+	int width, height;
+	SDL_QueryTexture(win->layers[num_layer]->texture, NULL, NULL, &width, &height);
+	SDL_Surface* surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
+	SDL_RenderReadPixels(win->ren, NULL, surface->format->format, surface->pixels, surface->pitch);
+	type = select_file_type(win);
 	if (type == PNG)
 	    IMG_SavePNG(surface, path);
 	else if (type == BMP)
